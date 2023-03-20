40c3a518ce93d9774b845976a54e407dfccf190c
Usman-Malikk <103499820+Usman-Malikk@users.noreply.github.com>
2023-03-20 20:24:48 +0500

    Add files via upload

---
+++ src/App/Assets/Images/Images.js
@@ -0,0 +1,11 @@
+import Logo from "./Logo.png";
+import GooglePlay from "./GoogleStore.png";
+import ApplePlay from "./AppleStore.png";
+import Map from "./Map.PNG";
+
+export const Images = {
+  Logo: Logo,
+  GoogleLogo: GooglePlay,
+  Applelogo: ApplePlay,
+  Map: Map,
+};
